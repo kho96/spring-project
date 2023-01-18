90d2b1c5bfb5041d07082cda1ed3422fb4b6b92c
suffolk066 <70433204+suffolk066@users.noreply.github.com>
2023-01-18 17:28:10 +0900

    initial commit

--- spring-project/src/main/java/com/kh/project/controller/AdminController.java
+++ spring-project/src/main/java/com/kh/project/controller/AdminController.java
@@ -1,27 +1,21 @@
 package com.kh.project.controller;
 
 
-import java.io.Console;
 import java.util.List;
-
-import javax.mail.Session;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.kh.project.service.AdminMovieService;
-import com.kh.project.vo.MovieVo;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.kh.project.service.AnnService;
 import com.kh.project.service.QnaCommentService;
 import com.kh.project.service.QnaService;
 import com.kh.project.vo.AnnVo;
+import com.kh.project.vo.MovieVo;
 import com.kh.project.vo.PagingDto;
 import com.kh.project.vo.QnaCommentVo;
 import com.kh.project.vo.QnaVo;
@@ -69,6 +63,11 @@ public class AdminController {
 		model.addAttribute("qnaCommentVo", qnaCommentVo);
 		model.addAttribute("page", page);
 		return "admin_qna_board";
+	}
+	
+	@RequestMapping(value = "/member", method = RequestMethod.GET)
+	public String showMember() {
+		return "admin_member";
 	}
 	
 	@ResponseBody
